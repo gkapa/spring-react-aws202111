47fb2f757a83e1f765aed703fe267ecf1a2772ed
unknown <s-han@seaos.co.jp>
2021-11-13 16:10:57 +0900

    build test

--- src/main/java/com/carma/hanppopen/controller/StudentController.java
+++ src/main/java/com/carma/hanppopen/controller/StudentController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/students")
+@RequestMapping("/api/students")
 public class StudentController {
     private final StudentService studentService;
 
